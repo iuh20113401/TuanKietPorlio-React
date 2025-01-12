d1e4546025d631a60747f44832605c65aeb0a2f3
Nguyễn Tuấn Kiệt <kiet5454aa@gmail.com>
2025-01-12 18:54:38 +0700

    update social information

--- src/sections/footer/data.jsx
+++ src/sections/footer/data.jsx
@@ -1,20 +1,26 @@
-import {AiOutlineInstagram} from 'react-icons/ai'
-import {AiOutlineTwitter} from 'react-icons/ai'
-import {AiOutlineDribbble} from 'react-icons/ai'
-import {AiFillGithub} from 'react-icons/ai'
+import { AiOutlineInstagram } from "react-icons/ai";
+import { AiOutlineTwitter } from "react-icons/ai";
+import { AiOutlineDribbble } from "react-icons/ai";
+import { AiFillGithub } from "react-icons/ai";
 
 export const links = [
-    {id: 1, link: '#', title: 'Home'},
-    {id: 2, link: '#about', title: 'About'},
-    {id: 3, link: '#services', title: 'Services'},
-    {id: 4, link: '#portfolio', title: 'Portfolio'},
-    {id: 5, link: '#contact', title: 'Contact'}
-]
-
+  { id: 1, link: "#", title: "Home" },
+  { id: 2, link: "#about", title: "About" },
+  { id: 3, link: "#services", title: "Services" },
+  { id: 4, link: "#portfolio", title: "Portfolio" },
+  { id: 5, link: "#contact", title: "Contact" },
+];
 
 export const socials = [
-    {id: 1, link: 'https://instagram.com', icon: <AiOutlineInstagram/>},
-    {id: 2, link: 'https://twitter.com', icon: <AiOutlineTwitter/>},
-    {id: 3, link: 'https://dribbble.com', icon: <AiOutlineDribbble/>},
-    {id: 4, link: 'https://github.com', icon: <AiFillGithub/>}
-]
+  { id: 1, icon: <HiOutlineMail />, link: "mailto:kiet5454aa@gmail.com" },
+  {
+    id: 2,
+    icon: <RiMessengerLine />,
+    link: "https://m.me/nguyen.kiet.232605/",
+  },
+  {
+    id: 3,
+    icon: <FaLinkedin />,
+    link: "https://www.linkedin.com/in/deri0405/",
+  },
+];
--- src/sections/header/data.jsx
+++ src/sections/header/data.jsx
@@ -1,13 +1,18 @@
-import {AiOutlineInstagram} from 'react-icons/ai'
-import {AiOutlineTwitter} from 'react-icons/ai'
-import {AiOutlineDribbble} from 'react-icons/ai'
-import {AiFillGithub} from 'react-icons/ai'
+import { AiFillFacebook, AiFillLinkedin } from "react-icons/ai";
+import { AiFillGithub } from "react-icons/ai";
 
 const data = [
-    {id: 1, link: 'https://instagram.com', icon: <AiOutlineInstagram/>},
-    {id: 2, link: 'https://twitter.com', icon: <AiOutlineTwitter/>},
-    {id: 3, link: 'https://dribbble.com', icon: <AiOutlineDribbble/>},
-    {id: 4, link: 'https://github.com', icon: <AiFillGithub/>}
-]
+  {
+    id: 1,
+    link: "https://www.linkedin.com/in/deri0405/",
+    icon: <AiFillLinkedin />,
+  },
+  {
+    id: 2,
+    link: "https://www.facebook.com/nguyen.kiet.232605/",
+    icon: <AiFillFacebook />,
+  },
+  { id: 3, link: "https://github.com/iuh20113401", icon: <AiFillGithub /> },
+];
 
-export default data
+export default data;
